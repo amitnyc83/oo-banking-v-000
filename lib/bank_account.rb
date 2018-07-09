af987a373dfb8ecbc17b8cde17c1a3b9b4cac46e
Amit Singh <amit_nyc@live.com>
2018-07-09 18:12:18 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,7 +7,7 @@ attr_accessor :balance, :status
 
 attr_reader   :name
 
-def initialize(name, @status = "open", @balance = 1000)
+def initialize(name, status = "open", balance = 1000)
   @name = name
   @balance = balance
   @status = status
