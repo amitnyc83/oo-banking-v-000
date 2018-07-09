a9c390cbe093df9c36e3c61dbca8297230613403
Amit Singh <amit_nyc@live.com>
2018-07-09 18:28:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ def execute_transaction
   if valid? && sender.balance >  amount && self.status = "pending"
     sender.withdrawal(self.amount)
     receiver.deposit(self.amount)
-    self.status = 'complete'
+    self.status = "complete"
   else
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
