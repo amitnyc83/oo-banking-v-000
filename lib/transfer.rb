e6c43a392e48bf83d6b698968f1634d1f771ddfb
Amit Singh <amit_nyc@live.com>
2018-07-09 18:36:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,27 +15,27 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
-# def execute_transaction
-#   if valid? && sender.balance >  amount && self.status = "pending"
-#     sender.withdrawal(self.amount)
-#     receiver.deposit(self.amount)
-#     self.status = "complete"
-#   else
-#     self.status = "rejected"
-#     "Transaction rejected. Please check your account balance."
-#   end
-# end
-
 def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.withdrawal(self.amount)
-      receiver.deposit(self.amount)
-      self.status = "complete"
-    else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
+  if valid? && sender.balance >  amount && self.status == "pending"
+    sender.withdrawal(self.amount)
+    receiver.deposit(self.amount)
+    self.status = "complete"
+  else
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end
+end
+
+# def execute_transaction
+#     if valid? && sender.balance > amount && self.status == "pending"
+#       sender.withdrawal(self.amount)
+#       receiver.deposit(self.amount)
+#       self.status = "complete"
+#     else
+#       self.status = "rejected"
+#       "Transaction rejected. Please check your account balance."
+#     end
+#   end
 
 
 end
